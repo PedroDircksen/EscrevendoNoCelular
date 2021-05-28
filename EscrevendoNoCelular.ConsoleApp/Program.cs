19cfdc567366728a43ee3f0d0799b6503157f4ea
Pedro Dircksen <dircksenpedro@gmail.com>
2021-05-28 17:13:19 -0300

    first commit

---
+++ EscrevendoNoCelular.ConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EscrevendoNoCelular.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ConversorDeCaractere conversor = new ConversorDeCaractere();
+            //char a = conversor.dois[0];
+            Teclado teclado = new Teclado("SEMPRE ACESSO O DOJOPUZZLES");
+
+            Console.WriteLine(teclado.resultado);
+            Console.ReadLine();
+        }
+    }
+}
